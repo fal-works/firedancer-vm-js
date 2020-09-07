6266fa43334744a3c53eb9b94fa4c41110e2b0cc
FAL <falworks.contact@gmail.com>
2020-09-07 23:04:48 +0900

    add license comment

---
+++ pre-publish/license-comment.js
@@ -0,0 +1,15 @@
+/**
+ * FiredancerVM.js
+ * https://github.com/fal-works/firedancer-vm-js
+ *
+ * @copyright 2020 FAL Works
+ * @license MIT
+ * @version *.*.*
+ */
+/**
+ * The following software may be included in this product:
+ *
+ * The Haxe Standard Library
+ * © 2005-2016 Haxe Foundation,
+ * used under MIT License (https://haxe.org/foundation/open-source.html)
+ */
