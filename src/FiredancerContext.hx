13f713e291dc6ed7d4c6fa7f40eb533b15a70f7e
FAL <falworks.contact@gmail.com>
2020-09-07 17:40:11 +0900

    minor

--- src/FiredancerContext.hx
+++ src/FiredancerContext.hx
@@ -5,10 +5,10 @@ import firedancer.vm.Thread;
 typedef FiredancerContext = {
 	programPackage:ProgramPackage,
 	habitableZone:RectangleBounds,
-	defaultTargetPositionRef:PositionRef,
-	onEnd:(actor:Actor, endCode:Int) -> Void,
 	getGroupOnFire:(fireCode:Int) -> ActorGroup,
 	onFire:(actor:Actor, fireCode:Int) -> Void,
+	defaultTargetPositionRef:PositionRef,
+	onEnd:(actor:Actor, endCode:Int) -> Void,
 	onGlobalEvent:(glotalEventCode:Int) -> Void,
 	onLocalEvent:(localEventCode:Int, x:Float, y:Float, thread:Thread, originPositionRef:Maybe<PositionRef>, targetpositionRef:PositionRef) -> Void,
 	defaultDraw:(actor:Actor) -> Void
@@ -26,14 +26,14 @@ class FiredancerContextTools {
 	public static function fill(ctx:FiredancerContext):FiredancerContext {
 		if (isNone(ctx.habitableZone))
 			ctx.habitableZone = RectangleBounds.fromSize(640.0, 480.0, 0.0, true, true);
-		if (isNone(ctx.defaultTargetPositionRef))
-			ctx.defaultTargetPositionRef = {x: 0.0, y: 0.0};
-		if (isNone(ctx.onEnd))
-			ctx.onEnd = (_, _) -> {};
 		if (isNone(ctx.getGroupOnFire))
 			ctx.getGroupOnFire = (_) -> throw "Missing getGroupOnFire() in the current context.";
 		if (isNone(ctx.onFire))
 			ctx.onFire = (_, _) -> {};
+		if (isNone(ctx.defaultTargetPositionRef))
+			ctx.defaultTargetPositionRef = {x: 0.0, y: 0.0};
+		if (isNone(ctx.onEnd))
+			ctx.onEnd = (_, _) -> {};
 		if (isNone(ctx.onGlobalEvent))
 			ctx.onGlobalEvent = (_) -> {};
 		if (isNone(ctx.onLocalEvent))
